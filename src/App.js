429bc05aa044c84373ae0d1dc0167ff9897dc1df
nassarahmad <ahmad.m.nassarr@gmail.com>
2024-06-11 14:22:14 +0300

    create functions in app and crud

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
 import "./App.css";
+import React, { useState } from 'react';
+import GroupForm from './components/GroupForm';
+import GroupList from './components/GroupList';
+import GroupEdit from './components/GroupEdit';
+const App = () => {
+  const [groups, setGroups] = useState([]);
+  const [editingGroup, setEditingGroup] = useState(null);
 
-function App() {
+  const addGroup = (group) => {
+    setGroups([...groups, group]);
+  };
+
+  const deleteGroup = (id) => {
+    setGroups(groups.filter(group => group.id !== id));
+  };
+
+  const editGroup = (group) => {
+    setEditingGroup(group);
+  };
+
+  const updateGroup = (updatedGroup) => {
+    setGroups(groups.map(group => group.id === updatedGroup.id ? updatedGroup : group));
+    setEditingGroup(null);
+  };
+
+  const cancelEdit = () => {
+    setEditingGroup(null);
+  };
+
   return (
     <div className="App">
-      <h1>Hello, World!</h1>
+      <h1>Group Management</h1>
+      {editingGroup ? (
+        <GroupEdit 
+          group={editingGroup}
+          updateGroup={updateGroup}
+          cancelEdit={cancelEdit}
+        />
+      ) : (
+        <GroupForm addGroup={addGroup} />
+      )}
+      <GroupList 
+        groups={groups}
+        deleteGroup={deleteGroup}
+        editGroup={editGroup}
+      />
     </div>
   );
-}
+};
 
 export default App;
